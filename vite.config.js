45357ab4d741d87495985d2eeed06d393d709805
discoballonmyhead <sagnikdaslearns@gmail.com>
2024-09-03 21:13:35 -0500

    vercel try

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/StatelessLabslogo/"
-})
+export default defineConfig(({ mode }) => {
+  const envFilePath = resolve(__dirname, `config/env/.env.${mode}`);
+  dotenv.config({ path: envFilePath });
+
+  const repoName = process.env.VITE_GH_REPO || '';
+
+  return {
+    plugins: [react()],
+    base: mode === 'github' ? `/${repoName}/` : '/',
+  };
+});
